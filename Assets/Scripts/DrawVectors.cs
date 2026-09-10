8004258d68b68d22e3d6dac61772cbbe89d590a6
escaland <escaland@OA-S141-18>
2026-09-10 17:53:58 -0400

    restarting

---
+++ Assets/Scripts/DrawVectors.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class DrawVectors : MonoBehaviour
+{
+    Vector2 dVector = new Vector2(0, 1);
+    Vector2 eVector = new Vector2(3, -2);
+    void Update()
+    {
+        Debug.DrawLine(Vector2.zero, dVector, Color.yellow);
+        Debug.DrawLine(Vector2.zero, eVector, Color.grey);
+        // Create two Vector2 variables, "dVector" and "eVector"
+        // Set their values to dVector: (0, 1) and eVector: (3, -2)
+
+        // Use Debug.DrawLine to draw a yellow vector starting at the origin and ending at dVector.
+        // Use Debug.DrawLine to draw a grey vector starting at the origin and ending at eVector.
+    }
+}
